e12bf26d449f7bce3b2db4aae752a081bd410e51
nailanurul <naila.nurul@student.alhazen.academy>
2025-02-21 11:02:10 +0700

    project miggu lalu

---
+++ phyton.py
@@ -0,0 +1,16 @@
+nama_lengkap = input("Naila Nurul Misbah:")
+email = Input("neilongjuno@gmail.com")
+nomor_hp = input("085951508361")
+
+
+print("=" * 40)
+print("Formulir pendaftaran akun siswa")
+print("=" * 40)
+print("\nNama lengkap :", Nama_lengkap)
+print("Email         :",Email)
+print("Nomor handphone:,nomor_hp")
+  
+
+print("\nHi!",nama_lengkap)
+print("Selamat! Email",email, "Berhasil terdaftar di sistem kami")
+print("\nLink untuk login ke aplikasi dikirim ke nomor",nomor_hp)
