21e606a60a02b3ef9a49294152276c366029ee3e
nailanurul <naila.nurul@student.alhazen.academy>
2025-05-02 11:04:52 +0700

    Add files via upload

---
+++ Naila.py
@@ -0,0 +1,15 @@
+numbers = range(0,10)
+print(list(numbers))
+numbers[1]
+numbers[9]
+
+
+
+numbers = range(1,21)
+print(numbers[6])
+
+
+numbers = list(range(2,21,2))
+print(numbers[:5])
+print(numbers[2:6])
+print(numbers[2:8:3])
