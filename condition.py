9bae76996969e760734176cea6d21758d4817750
nailanurul <naila.nurul@student.alhazen.academy>
2025-05-16 10:32:49 +0700

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,10 @@
+score = 75
+score =int(score)
+
+if score >= 80 :
+    print("score A")
+elif score < 85:
+    print("score B")
+else:
+    print("nscore C")    
+    
---
+++ loop.py
@@ -0,0 +1,13 @@
+
+for i in range(1,6):
+    print(1)
+    
+jumlah = input("input count = ")
+jumlah = int(jumlah)
+
+count = 1
+while count <= jumlah:
+    print("Halo")
+    count += 1
+
+
