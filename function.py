72474f03d6334e00fb6a72ab46cf5d2e5abb2c76
nailanurul <naila.nurul@student.alhazen.academy>
2025-08-08 13:45:12 +0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,39 @@
+def sayHello():
+    print("Selamat datang")
+
+def sayHello():
+    return "Selamat Datang"
+
+def sayHello(name):
+    return "Selamat Datang" + name
+
+def addition(nim1 , num2):
+    result = num1 + num2
+    print(num1,"+",num2,"=",result)
+
+def sayHello(name):
+        return "Selamat Datang" + name
+
+def sayHello(name=""):
+     return "Selamat Datang" + name
+
+def fullname(fname, lname=""):
+     return fname + "" + lname
+
+def addition(*numbers):
+     result = 0
+
+     for n in numbers:
+          
+          return result
+     
+     deffullname(**kwargs)
+     values = kwargs.values()
+     print("".join(values))
+           
+     fullname ( a="Ahmad,b="Dwi", c="Santoso)
+
+
+
+    
+
