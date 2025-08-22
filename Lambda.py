cb7c5512097a6a53f115259480e93391adcfb7f5
nailanurul <naila.nurul@student.alhazen.academy>
2025-08-22 12:04:57 +0700

    Add files via upload

---
+++ Lambda.py
@@ -0,0 +1,11 @@
+lambda name : print("Selamat Datang", name)
+
+sayHello = lambda name : print("Selamat Datang", name)
+
+sayHello("Ahmad")
+
+(lambda : print("Selamat Datang"))()
+
+(lambda name : print("Selamat Datang", name))("Ahmad")
+
+(lambda name ="" : print("Selamat Datang", name))()
