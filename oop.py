02a245c9b0240355dd4abe219bf263796f189357
nailanurul <naila.nurul@student.alhazen.academy>
2025-09-26 14:00:44 +0700

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,14 @@
+#Soal oop
+#1. b. object oriented programming
+#2. c. class adlh cetak biru untuk membuat objek
+
+#Isian singkat
+#3. class berupa kategori 
+     #sedangkan object berupa variable seperti ciri yang dimiliki pada sebuah object
+#4. 
+class Hewan:
+    Kucing = Hewan("Max", "Mimi", "Kucing Anggora" )
+    Kucing.makesound("meowing")
+    Kucing.eat("fish")
+
+#5. a.untuk menampilkan object di layar
