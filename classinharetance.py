24b9a60d78968424c73e77567faaf1c02907d712
nailanurul <naila.nurul@student.alhazen.academy>
2025-10-03 10:32:56 +0700

    Add files via upload

---
+++ classinharetance.py
@@ -0,0 +1,44 @@
+class Person:
+    name = ""
+
+    def__init__(self, name);
+    self.name = name
+
+    def getInfo(self):
+        print("This person's name is",self.name)
+
+        class Sdudent(Person):
+            pass
+
+student = Student("Ahmad")
+student.getinfo()
+
+class Person:
+    name = ""
+
+    def__init__(self,name);
+
+    def getInfo(self):
+        print("This person's is", self.name)
+
+        class Student(person):
+            def__init__(self,name,nis);
+            super().__init__(name)
+            self.nis = nis
+
+Person1 = Person2("Ahmad")
+Student1 = Student("Beni", "202111243")
+
+class Student(Person):
+
+    nis = ""
+
+    def__init__(self,name,nis);
+    super().__init__(name)
+    self.nis = nis
+
+    def study(subject):
+        print(self.name, "is studying", subject)
+
+Student1 = Student("Beni", "202111243")  
+student1.study("Math")      
