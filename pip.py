3f89bbd29213aeacdfcfdd116d4e46a652ed65dd
nailanurul <naila.nurul@student.alhazen.academy>
2025-10-17 13:32:26 +0700

    Add files via upload

---
+++ pip.py
@@ -0,0 +1,7 @@
+from stringcolor import *
+
+print(cs("Selamat Datang", "blue", "white"))
+print(bold("Selamat Datang"))
+print(underline("Selamat Datang"))
+print(underline("Selamat Datang").bold())
+print(cs("Selamat Datang", "blue", "yellow").underline())
